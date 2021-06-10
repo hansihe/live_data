219d096749313cf9eda752edef5dddcdd438ad29
Hans Elias B. Josephsen <me@hansihe.com>
2021-06-10 23:24:11 +0200

    cleanup old prototype code

--- lib/phoenix_data_view/tracked/compiler2/ir/block.ex
+++
@@ -1,3 +0,0 @@
-defmodule Phoenix.DataView.Tracked.Compiler2.IR.Block do
-  defstruct id: nil, args: [], body: nil
-end
--- lib/phoenix_data_view/tracked/compiler2/ir/op.ex
+++
@@ -1,9 +0,0 @@
-defprotocol Phoenix.DataView.Tracked.Compiler2.IR.Op do
-
-  def outgoing(ir)
-
-  def values(ir)
-
-  def single_cont(ir)
-
-end
